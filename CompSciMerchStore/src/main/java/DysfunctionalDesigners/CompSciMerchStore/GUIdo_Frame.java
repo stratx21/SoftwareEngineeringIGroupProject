f35400cfa25f3c01d280aad51cfd456d8475473a
Josh Holland <stratx21@users.noreply.github.com>
2019-03-07 18:10:24 -0600

    Added panel for homepage, functions for frame

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_Frame.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_Frame.java
@@ -3,5 +3,12 @@ package DysfunctionalDesigners.CompSciMerchStore;
 import javax.swing.JFrame;
 
 public class GUIdo_Frame extends JFrame{
-
+	
+	public GUIdo_Frame() {
+		
+	}
+	
+	public void initialize() {
+		
+	}
 }
---
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_Homescreen.java
@@ -0,0 +1,9 @@
+package DysfunctionalDesigners.CompSciMerchStore;
+
+import javax.swing.JPanel;
+
+public class GUIdo_Homescreen extends JPanel{
+	
+	
+
+}
