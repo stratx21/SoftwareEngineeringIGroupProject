6e016cae74e16770ccb9e25120c0489be9ec4b43
Josh Holland <stratx21@users.noreply.github.com>
2019-03-10 03:06:51 -0500

    added drawing tests for the home screen

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_Homescreen.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_Homescreen.java
@@ -1,8 +1,14 @@
 package DysfunctionalDesigners.CompSciMerchStore;
 
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class GUIdo_Homescreen extends GUIdo_CPanel{
 	
-	
+	@Override
+	public void paintComponent(Graphics g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(50, 50, 100, 100);
+	}
 
 }
