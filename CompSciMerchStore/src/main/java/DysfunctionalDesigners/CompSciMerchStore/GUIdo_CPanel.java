1261b9eb01dc0dadef33138bc48505a4af1cfb24
Josh Holland <stratx21@users.noreply.github.com>
2019-03-10 20:40:30 -0500

    Added base functionality for formats and page lengths

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_CPanel.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_CPanel.java
@@ -1,10 +1,20 @@
 package DysfunctionalDesigners.CompSciMerchStore;
 
+import java.awt.GridBagLayout;
+
 import javax.swing.JPanel;
 
 public class GUIdo_CPanel extends JPanel{
 	
+	private int page_length = 400;
+	
 	public GUIdo_CPanel() {
 		this.setLayout(null);
+		this.setAutoscrolls(true);
+	}
+	
+	public GUIdo_CPanel(int page_length_pixels) {
+		this();
+		this.page_length=page_length_pixels;
 	}
 }
