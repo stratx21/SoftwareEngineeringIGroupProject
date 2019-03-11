5c2705d61c8aef4d5c8455e73babb462dfcc0ff9
Josh Holland <stratx21@users.noreply.github.com>
2019-03-10 20:37:07 -0500

    Search bar updates, home button updates, homescreen testing and setup

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_CToolbar.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_CToolbar.java
@@ -2,22 +2,62 @@ package DysfunctionalDesigners.CompSciMerchStore;
 
 import java.awt.Color;
 
+import javax.swing.JTextField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class GUIdo_CToolbar extends GUIdo_CPanel{
 	
 	private int x,y;
 	private int width, height;
 	
-	public GUIdo_CToolbar(int x, int y, int width, int height) {
+	private JTextField searchBar;
+	
+	public GUIdo_CToolbar(int x, int y, int width, int height, final ActionListener done) {
+//		super();
+		//general setup:
 		this.setSize(width, height);
 		this.width=width;
 		this.height=height;
-		this.setBackground(Color.DARK_GRAY);
+		this.setBackground(Color.PINK);
+		
+		//home button:
+		
+		GUIdo_CButton home_button = new GUIdo_CButton(x+height/10,y+height/10,height*8/10,height*8/10,"HOME");
+		home_button.setActionListener_clicked(done);
+		home_button.setBackground(Color.YELLOW);
 		
 		
-		GUIdo_CButton home_button = new GUIdo_CButton(x,y,height,height,"HOME");
+		//search bar:
+		
+		searchBar = new JTextField("search");
+		
+		searchBar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//TODO: implement search algorithm here:
+				System.out.println("SEARCH invoked with string \"" + searchBar.getText() + "\"");
+				
+				//use searchBar.getText() to get the String of text entered to search
+				
+				
+				done.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED,"search"));
+			}
+		});
+		searchBar.setSize(width-height, height/2);
+		searchBar.setLocation(height, height/4);
+		
+		searchBar.setBackground(new Color(255,102,167));
+		
 		
 		this.add(home_button);
+		this.add(searchBar);
 		
+		home_button.repaint();
+		
+//		this.setVisible(true);
+		
+		this.repaint();
 	}
 	
 }
--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_Homescreen.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_Homescreen.java
@@ -1,14 +1,22 @@
 package DysfunctionalDesigners.CompSciMerchStore;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 
 public class GUIdo_Homescreen extends GUIdo_CPanel{
 	
+	public GUIdo_Homescreen(){
+		super();
+		this.setPreferredSize(new Dimension(this.getWidth(), 1500));
+		this.repaint();
+		this.setVisible(true);
+	}
+	
 	@Override
 	public void paintComponent(Graphics g) {
 		g.setColor(Color.BLUE);
-		g.fillRect(50, 50, 100, 100);
+		g.fillRect(50, 50, 100, 1000);
 	}
 
 }
