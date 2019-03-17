473fec7dbeaaa396a0f0807ff9e0b920a2062a7d
Em Lakin <emlakin21@gmail.com>
2019-03-16 19:12:44 -0500

    implemented user class

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/User.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/User.java
@@ -1,5 +1,100 @@
 package DysfunctionalDesigners.CompSciMerchStore;
 
 public class User {
-
+	String email;
+	String motherMaidenName;
+	String userName;
+	String password;
+	String name;
+	Integer userID;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMotherMaidenName() {
+		return motherMaidenName;
+	}
+	public void setMotherMaidenName(String motherMaidenName) {
+		this.motherMaidenName = motherMaidenName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public User(String email, String motherMaidenName, String userName, String password, String name, int userID) {
+		super();
+		this.email = email;
+		this.motherMaidenName = motherMaidenName;
+		this.userName = userName;
+		this.password = password;
+		this.name = name;
+		this.userID = userID;
+	}
+	public User(String[] d) {
+		this.email = d[0];
+		this.motherMaidenName = d[1];
+		this.userName = d[2];
+		this.password = d[3];
+		this.name = d[4];
+		this.userID = Integer.parseInt(d[5]);
+	}
+	
+	public String toStringDisplay() {
+		return "User [email=" + email + ", motherMaidenName=" + motherMaidenName + ", userName=" + userName
+				+ ", password=" + password + ", name=" + name + ", userID=" + userID + "]";
+	}
+	
+	public String toStringFile() {
+		String jsonFormat = "";
+		jsonFormat += "{\n";
+		
+		
+		jsonFormat += "\t\"email\"    : \"" + this.email    + "\",\n";
+		jsonFormat += "\t\"motherMaidenName\" : \"" + this.motherMaidenName + "\",\n";
+		jsonFormat += "\t\"username\" : \"" + this.userName + "\",\n";
+		jsonFormat += "\t\"password\" : \"" + this.password + "\",\n";
+		jsonFormat += "\t\"userID\"   : " + this.userID   + ",\n";
+		
+		
+		
+		
+		jsonFormat+="}";
+		return jsonFormat;
+	}
+	
+	public boolean isAdmin() {
+		String id = Integer.toString(this.userID);
+		if(id.charAt(0) == '4') {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	
+	
+	
 }
