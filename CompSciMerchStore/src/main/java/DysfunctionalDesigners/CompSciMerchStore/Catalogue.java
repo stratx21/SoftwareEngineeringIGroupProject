c84499e2afa700c9434486b3950af310dda647f9
etdickey <etdickey@gmail.com>
2019-03-17 23:24:34 -0500

    Updated to java 1.8 and finished the ItemInfo, Sale, LineItem and Catalogue classes except for search.

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/Catalogue.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/Catalogue.java
@@ -1,18 +1,153 @@
 package DysfunctionalDesigners.CompSciMerchStore;
 
-import java.util.Set;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class Catalogue {
-	private static Set<ItemInfo> catalogue;
+public class Catalogue {//all should be static as the top-level class unfortunately cannot be
+	private static Map<Integer, ItemInfo> catalogue = new HashMap<Integer, ItemInfo>();
 	private static int numItems = 0;
 	
+//////////////////modifiers
+	/**
+	 * Adds an item (or replaces an old one) to the catalogue
+	 * 
+	 * @param e the item to add
+	 */
 	public static void addItem(ItemInfo e) {
-		catalogue.add(e);
-		numItems++;
+		if(catalogue.put(e.getItemID(), e) == null) {
+			numItems++;
+		}
 	}
 	
+	/**
+	 * Removes an item from the catalogue if present.
+	 * 
+	 * @param itemID the item to remove
+	 */
 	public static void removeItem(int itemID) {
-		//TODOOOOOOOO
+		if(catalogue.remove(itemID) != null) {
+			numItems--;
+		}
 	}
-	//all should be static as the top-level class unfortunately cannot be
+	
+	/**
+	 * Decreases the quantity of the given item if the amount is valid.
+	 * 
+	 * @param itemID the item to decrease
+	 * @param amount the amount to decrease by
+	 */
+	public static void decreaseQuantity(int itemID, int amount) {
+		if(checkAmount(itemID, amount)) {
+			try {
+				catalogue.get(itemID).decreaseAmount(amount);
+			} catch (Exception e) {
+				System.err.println("ERROR: checkAmount failed");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * Increases the quantity of the given item if the amount is valid.
+	 * 
+	 * @param itemID the item to increase
+	 * @param amount the amount to increase by
+	 */
+	public static void increaseQuantity(int itemID, int amount) {
+		if(catalogue.containsKey(itemID)) {
+			catalogue.get(itemID).increaseAmount(amount);
+		}
+	}
+	
+	/**
+	 * Disables the item
+	 * @param itemID the item to disable
+	 */
+	public void disableItem(int itemID) {
+		if(catalogue.containsKey(itemID)) {
+			catalogue.get(itemID).disable();
+		}
+	}
+	
+	/**
+	 * Enables the item
+	 * @param itemID the item to enable
+	 */
+	public void enableItem(int itemID) {
+		if(catalogue.containsKey(itemID)) {
+			catalogue.get(itemID).enable();
+		}
+	}
+	
+	public void updateItem(int itemID, ItemInfo info) {
+		catalogue.put(itemID, info);
+	}
+	
+//////////////////accessors
+	/**
+	 * @param itemID the item to get
+	 * @return the ItemInfo for that item
+	 */
+	public static ItemInfo getItem(int itemID) {
+		return catalogue.get(itemID);
+	}
+	
+	/**
+	 * @return the number of unique items in the catalogue
+	 */
+	public static int getNumItems() { return numItems; }
+	
+	/**
+	 * @param itemID item to check
+	 * @return if it's enabled
+	 */
+	public static boolean isEnabled(int itemID) {
+		if(catalogue.containsKey(itemID)) {
+			return catalogue.get(itemID).isEnabled();
+		}
+		return false;
+	}
+	
+//////////////////others
+	/**
+	 * Checks if the given amount can be taken from the item given.
+	 * 
+	 * @param itemID the item to check
+	 * @param amount the amount to withdraw
+	 * @return if that amount can be taken from that item
+	 */
+	public static boolean checkAmount(int itemID, int amount) {
+		ItemInfo temp = catalogue.get(itemID);
+		if(temp != null) {
+			return (amount >= 0 && amount <= temp.getStock());
+		}
+		return false;
+	}
+	
+	/**
+	 * Checks to see if a promo code is valid.
+	 * 
+	 * @param code the code to check
+	 * @return if the code is valid
+	 */
+	public static boolean checkPromoCode(String code) {
+		if(code.length() >= ItemInfo.EXTENDED_ID_LENGTH) {
+			int itemID = Integer.parseInt(code.substring(code.length()-ItemInfo.EXTENDED_ID_LENGTH));
+			return catalogue.get(itemID).hasPromoCode(code);
+		}
+		return false;
+	}
+	
+	/**
+	 * Searches the catalogue for a keyword and pulls up items like it.
+	 * 
+	 * @param keywords the string to search for
+	 * @return the list of ItemInfos which match the keywords
+	 */
+	public static List<ItemInfo> search(String keywords){
+		//TODO: Kind of auto-generated method stub
+		return null;
+	}
+	
 }
--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/ItemInfo.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/ItemInfo.java
@@ -1,23 +1,205 @@
 package DysfunctionalDesigners.CompSciMerchStore;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ItemInfo {
-	static int NEXT_ID = 0;
-	String description, displayName;
-	int itemID, stock, vendorID;
-	double saleDiscount;
-	Map<String, Double> promoDiscounts;
+	public static final int EXTENDED_ID_LENGTH = 5;
+	private static int NEXT_ID = 0;
+	private String description, displayName, extendedItemID;
+	private int itemID, stock, vendorID;
+	private double saleDiscount, price;
+	private Map<String, Double> promoDiscounts;
+	private boolean enabled = true;
 	
-	public ItemInfo(String description, String displayName, int stock, int vendorID) {
+	/**
+	 * this is for quick searching and comparison objects -- not meant to be a permanent object
+	 * @param ID a temp id to compare with
+	 */
+	public ItemInfo(int ID) {
+		this.itemID = ID;
+		this.description = "ERROR: This is a temporary ItemInfo and shouldn't be used for more than that";
+		this.displayName = "ERROR: This is a temporary ItemInfo and shouldn't be used for more than that";
+		this.extendedItemID = "ERROR: This is a temporary ItemInfo and shouldn't be used for more than that";
+		this.stock = -1;
+		this.vendorID = -1;
+		this.saleDiscount = -1;
+		this.price = -1;
+		this.promoDiscounts = null;
+	}
+	
+	/**
+	 * Creates an ItemInfo with the specified fields
+	 */
+	public ItemInfo(String description, String displayName, int stock, int vendorID, double price) {
 		super();
 		this.description = description;
 		this.displayName = displayName;
 		this.itemID = NEXT_ID++;///////////////////////////////////////////this will need to be changed to be incorporated into one of the files so new items don't get set to old item's numbers
+		this.extendedItemID = String.format("%0" + EXTENDED_ID_LENGTH, this.itemID);
 		this.stock = stock;
 		this.vendorID = vendorID;
+		this.saleDiscount = 0.0;
+		this.promoDiscounts = new HashMap<String, Double>();
+		this.price = price;
+	}
+
+	/**
+	 * This returns the total discount counting the sale discount and promo discounts.
+	 * 
+	 * @param promoKeys the list of promotion strings associated with this product
+	 * @return discount the decimal percent (.1 = 10%) off the product is
+	 */
+	public double getTotalDiscount(List<String> promoKeys) {
+		if(!this.promoDiscounts.isEmpty()) {
+			double total = saleDiscount;
+			
+			for(String key : promoKeys) {
+				if(this.promoDiscounts.containsKey(key)) {
+					total += this.promoDiscounts.get(key);
+				}
+			}
+			
+			return (total >= 1 ? 1 : total);
+		} else {
+			return saleDiscount;
+		}	
 	}
 	
+	/**
+	 * This returns the total discount counting only sale discount.
+	 * 
+	 * @return discount the decimal percent (.1 = 10%) off the product is
+	 */
+	public double getTotalDiscount() {
+		return saleDiscount;
+	}
+	
+	/**
+	 * This returns the total price counting the sale discount and promo discounts.
+	 * 
+	 * @return discount the decimal percent (.1 = 10%) off the product is
+	 */
+	public double getTotalPrice(List<String> promoKeys) {
+		return this.price - this.price*this.getTotalDiscount(promoKeys);
+	}
+	
+	/**
+	 * This returns the total price counting only sale discount.
+	 * 
+	 * @return discount the decimal percent (.1 = 10%) off the product is
+	 */
+	public double getTotalPrice() {
+		return this.price - this.price*this.getTotalDiscount();
+	}
+	
+	/**
+	 * This function allows you to generate and add a promo code.
+	 * 
+	 * @param keyword the promo keyword used to access this discount (prepended before the item id)
+	 * @param discount the amount off the discount adds
+	 * @return code the promo code generated that can be used to access this discount
+	 */
+	public String makePromoDiscount(String keyword, double discount) {
+		//key will be keyword+id
+		this.promoDiscounts.put(keyword + this.extendedItemID, (discount > 1 ? 1 : discount));
+		return keyword + this.extendedItemID;
+	}
+	
+	/**
+	 * This function allows you to update a promo code and will replace any previous discount.
+	 * 
+	 * @param key the promo key used to access this discount with the itemID appended
+	 * @param discount the amount off the discount adds
+	 */
+	public void updatePromoDiscount(String key, double discount) {
+		this.promoDiscounts.put(key, (discount > 1 ? 1 : discount));
+	}
+	
+	/**
+	 * This function allows the removal of one promo code
+	 * 
+	 * @param key the promo code to remove
+	 */
+	public void removePromoDiscount(String key) {
+		this.promoDiscounts.remove(key);
+	}
+	
+	/**
+	 * This function returns whether or not this item has the given promo code
+	 * @param code the promo code to check
+	 * @return whether or not the item has the given promo code
+	 */
+	public boolean hasPromoCode(String code) {
+		return this.promoDiscounts.containsKey(code);
+	}
+	
+	/**
+	 * This decreases the stock by amount (or to zero if greater).
+	 * @param amount the amount to reduce the stock by
+	 * @throws Exception If amount is > stock
+	 */
+	public void decreaseAmount(int amount) throws Exception { 
+		if(amount > this.stock) {
+			throw new Exception("ERROR: Not enough stock");
+		} else if(amount > 0){
+			this.stock -= amount;
+		}
+	}
+	
+	/**
+	 * This increases the stock by amount.
+	 * @param amount the amount to increase the stock by
+	 */
+	public void increaseAmount(int amount) { 
+		if(amount > 0) {
+			this.stock += amount;
+		}
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + itemID;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemInfo other = (ItemInfo) obj;
+		if (itemID != other.itemID)
+			return false;
+		return true;
+	}
+
+	///////getters and setters
+	public String getDescription() { return description; }
+	public String getDisplayName() { return displayName; }
+	public int getItemID() { return itemID; }
+	public String getExtendedItemID() { return extendedItemID; }
+	public int getStock() { return stock; }
+	public int getVendorID() { return vendorID; }
+	public double getSaleDiscount() { return saleDiscount; }
+	public Map<String, Double> getPromoDiscounts() { return promoDiscounts; }
+	public double getPrice() { return price; }
+	
+	public void enable() { this.enabled = true; }
+	public void disable() { this.enabled = false; }
+	public boolean isEnabled() { return this.enabled; }
+	public void setPrice(double price) { this.price = price; }
+	public void setDescription(String description) { this.description = description; }
+	public void setDisplayName(String displayName) { this.displayName = displayName; }
+	public void setStock(int stock) { this.stock = stock; }
+	public void setSaleDiscount(double saleDiscount) { this.saleDiscount = (saleDiscount > 1 ? 1 : saleDiscount); }
+	public void setPromoDiscounts(Map<String, Double> promoDiscounts) { this.promoDiscounts = promoDiscounts; }
 	
 	
 }
