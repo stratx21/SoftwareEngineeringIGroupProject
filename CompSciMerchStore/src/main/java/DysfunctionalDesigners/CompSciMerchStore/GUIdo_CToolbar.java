0c444221f2220399f3c7a2bb94629c7b3c64e92f
Josh Holland <stratx21@users.noreply.github.com>
2019-03-26 01:34:19 -0500

    Added functionality for disabling the buttons
    the buttons will be disabled when there is not a login or in other situations

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_CToolbar.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_CToolbar.java
@@ -2,14 +2,26 @@ package DysfunctionalDesigners.CompSciMerchStore;
 
 import java.awt.Color;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JTextField;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
 
 public class GUIdo_CToolbar extends GUIdo_CPanel{
 	
+	public boolean getButtons_disabled() {
+		return buttons_disabled;
+	}
+
 	private JTextField searchBar;
+	
+	private boolean buttons_disabled = false;
+	
+	private ArrayList<GUIdo_CButton> buttons = new ArrayList<>();
 	
 	public GUIdo_CToolbar(int x, int y, int width, int height, final ActionListener done) {
 //		super();
@@ -21,12 +33,21 @@ public class GUIdo_CToolbar extends GUIdo_CPanel{
 		
 		//home button:
 		
-		GUIdo_CButton cart = new GUIdo_CButton(width-height*4,y, height,height, "CART");
-		cart.setActionCommand("cart");
-		cart.setActionListener_clicked(done);
-		cart.setBackground(Color.YELLOW);
+		GUIdo_CButton cart = new GUIdo_CButton (width- height* 4,y , height, height, "CART" ) ;
+		cart. setActionCommand("cart");
+		cart. setActionListener_clicked( done);
+		cart.setBackground(Color. YELLOW);
 		
-		GUIdo_CButton home_button = new GUIdo_CButton(x,y,height,height,"HOME");
+		GUIdo_CButton home_button= null;
+		try  {
+			home_button  =  new GUIdo_CButton(x,y,height,height,
+					new ImageIcon(ImageIO.read(new File("src/main/resources/Toolbar/home (1).png"))),
+					new ImageIcon(ImageIO.read(new File("src/main/resources/Toolbar/home (2).png"))),
+					new ImageIcon(ImageIO.read(new File("src/main/resources/Toolbar/home (3).png"))));
+		} catch(Exception e) {
+			System.out.println("ERROR setting up home button initialization");
+			e.printStackTrace();
+		}
 		home_button.setActionCommand("home");
 		home_button.setActionListener_clicked(done);
 		home_button.setBackground(Color.YELLOW);
@@ -71,11 +92,29 @@ public class GUIdo_CToolbar extends GUIdo_CPanel{
 		this.add(login);
 		this.add(cart);
 		
+		buttons.add(home_button);
+		buttons.add(wishlist);
+		buttons.add(login);
+		buttons.add(cart);
+		
 		home_button.repaint();
 		
-//		this.setVisible(true);
-		
 		this.repaint();
+	}
+	
+	
+	public void disable_all_buttons() {
+		for(GUIdo_CButton button : buttons) {
+			button.disable();
+		}
+		buttons_disabled=true;
+	}
+	
+	public void enable_all_buttons() {
+		for(GUIdo_CButton button : buttons) {
+			button.enable();
+		}
+		buttons_disabled=false;
 	}
 	
 }
