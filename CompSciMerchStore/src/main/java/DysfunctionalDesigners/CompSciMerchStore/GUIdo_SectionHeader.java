721983feb10b799c3ba9b9e2375cb1d3c7d76e3d
Josh Holland <stratx21@users.noreply.github.com>
2019-04-07 14:13:08 -0500

    fixed mix up on professor names vs action commands

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_SectionHeader.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_SectionHeader.java
@@ -10,33 +10,33 @@ public class GUIdo_SectionHeader extends GUIdo_CPanel{
 	public GUIdo_SectionHeader(int x, int y, int width, int height, final ActionListener done) {
 		this.setSize(width, height);
 		
-		GUIdo_CButton section1 = new GUIdo_CButton(x+0*width/SECTIONS,y,width/SECTIONS,height,"cerny");
-		section1.setActionCommand("Dr. Cerny");
+		GUIdo_CButton section1 = new GUIdo_CButton(x+0*width/SECTIONS,y,width/SECTIONS,height,"Dr. Cerny");
+		section1.setActionCommand("cerny");
 		section1.setActionListener_clicked(done);
 		section1.setBackground(new Color(255,181,9));
 		
-		GUIdo_CButton section2 = new GUIdo_CButton(x+1*width/SECTIONS,y,width/SECTIONS,height,"booth");
-		section2.setActionCommand("Dr. Booth");
+		GUIdo_CButton section2 = new GUIdo_CButton(x+1*width/SECTIONS,y,width/SECTIONS,height,"Dr. Booth");
+		section2.setActionCommand("booth");
 		section2.setActionListener_clicked(done);
 		section2.setBackground(new Color(255,181,9));
 		
-		GUIdo_CButton section3 = new GUIdo_CButton(x+2*width/SECTIONS,y,width/SECTIONS,height,"fry");
-		section3.setActionCommand("Professor Fry");
+		GUIdo_CButton section3 = new GUIdo_CButton(x+2*width/SECTIONS,y,width/SECTIONS,height,"Professor Fry");
+		section3.setActionCommand("fry");
 		section3.setActionListener_clicked(done);
 		section3.setBackground(new Color(255,181,9));
 		
-		GUIdo_CButton section4 = new GUIdo_CButton(x+3*width/SECTIONS,y,width/SECTIONS,height,"hamerly");
-		section4.setActionCommand("Dr. Hamerly");
+		GUIdo_CButton section4 = new GUIdo_CButton(x+3*width/SECTIONS,y,width/SECTIONS,height,"Dr. Hamerly");
+		section4.setActionCommand("hamerly");
 		section4.setActionListener_clicked(done);
 		section4.setBackground(new Color(255,181,9));
 		
-		GUIdo_CButton section5 = new GUIdo_CButton(x+4*width/SECTIONS,y,width/SECTIONS,height,"aars");
-		section5.setActionCommand("Bald and Balding Aars");
+		GUIdo_CButton section5 = new GUIdo_CButton(x+4*width/SECTIONS,y,width/SECTIONS,height,"Bald and Balding Aars");
+		section5.setActionCommand("aars");
 		section5.setActionListener_clicked(done);
 		section5.setBackground(new Color(255,181,9));
 		
-		GUIdo_CButton section6 = new GUIdo_CButton(x+5*width/SECTIONS,y,width/SECTIONS,height,"maurer");
-		section6.setActionCommand("Maurer");
+		GUIdo_CButton section6 = new GUIdo_CButton(x+5*width/SECTIONS,y,width/SECTIONS,height,"Dr. Maurer");
+		section6.setActionCommand("maurer");
 		section6.setActionListener_clicked(done);
 		section6.setBackground(new Color(255,181,9));
 		
