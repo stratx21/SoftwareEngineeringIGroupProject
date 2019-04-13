94d9df2cac4ed3c7a385ef9424b3c3b6811ee7e4
Harrison Rogers <hman80@att.net>
2019-04-13 15:14:48 -0500

    added getUsers, getUserSales, getUsernames, etc.

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/UserDataController.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/UserDataController.java
@@ -1,4 +1,134 @@
 package DysfunctionalDesigners.CompSciMerchStore;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserDataController {
+    private static UserDataController instance = null;
+
+    public static UserDataController getInstance() {
+        if(instance == null) {
+            synchronized (UserDataController.class) {
+                if(instance == null) {
+                    instance = new UserDataController();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public List<User> getCustomers(List<String> usernames) {
+        List<User> users = new ArrayList<>();
+        List<String> validCustomerUsernames = getCustomerUsernames();
+
+        //Filter to only usernames existing in the usernames.txt file
+        usernames = usernames.stream()
+                .filter(e -> validCustomerUsernames.contains(e))
+                .collect(Collectors.toList());
+
+        //For each username, go to the user file and deserialize rom JSON to a Customer Object
+        //and add it to the list of User objects
+        for (String username : usernames) {
+            ObjectMapper mapper = new ObjectMapper();
+            Customer nextCust = null;
+            File usrFile = new File("./src/main/resources/UserData/" + username + ".json");
+
+            try {
+                nextCust = mapper.readValue(usrFile, Customer.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            users.add(nextCust);
+        }
+
+        return users;
+    }
+
+    public List<Sale> getUserSales(String username) throws Exception {
+        List<String> validUsernames = getCustomerUsernames();
+        List<Sale> pastSalesOfUser;
+
+        if(!validUsernames.contains(username)) {
+            throw new Exception("Username is either not a valid customer or an admin");
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        Customer cust = null;
+        cust = mapper.readValue(new File("./src/main/resources/UserData/" + username + ".json"),
+                Customer.class);
+        pastSalesOfUser = cust.getPastSales();
+
+        return pastSalesOfUser;
+    }
+
+    public List<Sale> getStoreSales() {
+        List<Sale> storeSales = new ArrayList<>();
+        List<String> validAdmins = getAdminUsernames();
+
+        
+    }
+
+    public List<String> getCustomerUsernames() {
+        List<String> usernames = new ArrayList<>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(
+                                    new File("./src/main/resources/UserData/customers.txt"))));
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+
+        addNamesToList(reader, usernames);
+
+        return usernames;
+    }
+
+    public List<String> getAdminUsernames() {
+        List<String> usernames = new ArrayList<>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(
+                                    new File("./src/main/resources/UserData/admins.txt"))));
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+
+        addNamesToList(reader, usernames);
+
+        return usernames;
+    }
+
+    private void addNamesToList(BufferedReader reader, List<String> listToAddTo) {
+        String line;
+        try {
+            while((line = reader.readLine()) != null) {
+                String[] split = line.split(" ");
+                if(split.length > 2) {
+                    continue;
+                }
+                listToAddTo.add(split[0]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
