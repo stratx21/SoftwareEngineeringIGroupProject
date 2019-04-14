15be857978340c114f78ec19ce67d8d1c9891b12
Mackenna Semeyn <Mackenna_Semeyn1@baylor.edu>
2019-04-14 01:55:28 -0500

    changes to payment frame

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_Payment.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_Payment.java
@@ -1,8 +1,12 @@
 package DysfunctionalDesigners.CompSciMerchStore;
 
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
+import javax.swing.JLabel;
 
 public class GUIdo_Payment extends GUIdo_CPanel implements ActionListener{
 	
@@ -13,10 +17,16 @@ public class GUIdo_Payment extends GUIdo_CPanel implements ActionListener{
   	    this.repaint();
 	}
 	
-	
 
 	private void drawScreen(Sale sale, int i) {
 		// TODO Auto-generated method stub
+		DecimalFormat df2 = new DecimalFormat("0.00");
+		JLabel label = new JLabel("Payment Information");
+		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setVerticalAlignment(JLabel.CENTER);
+		label.setFont(new Font("Cambria", Font.BOLD, 34));
+		this.add(label);
+		
 		
 	}
 
