3e742a08b5deab87ab9e8a1a1e01fb0d7aafc550
etdickey <etdickey@gmail.com>
2019-04-13 21:20:34 -0500

    Added logger to Sale class and implemented next_sale_id which populates at the beginning of the program and writes its new value back to the file every time it changes.  And removed unused imports.

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/Sale.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/Sale.java
@@ -1,9 +1,15 @@
 package DysfunctionalDesigners.CompSciMerchStore;
 
-import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Date;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.logging.Logger;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -15,7 +21,8 @@ import java.util.List;
 
 public class Sale {
 	static final double SHIPPING = 68, TAX = 0.08;//$, %
-	private static int nextSaleID = 0;
+	private static int nextSaleID = -1;
+	private static Logger logger = Logger.getLogger(Sale.class.getName());
 	
 	private Payment payment;
 	private Date dateTime;//finished when finalized
@@ -24,14 +31,39 @@ public class Sale {
 	private Address shippingAddr;
 	private boolean finalized = false;
 	
+	static {
+		try {
+			Scanner scan = new Scanner(new File("src/main/resources/NEXT_SALE_ID.txt"));
+			nextSaleID = scan.nextInt();
+			scan.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	
 	public Sale(int custID) {
 		this.customerID = custID;
 		this.saleID = nextSaleID++;
+		
+		try {
+			BufferedWriter bf = new BufferedWriter(new FileWriter(new File("src/main/resources/NEXT_SALE_ID.txt")));
+			bf.write(nextSaleID + "");
+			bf.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		this.itemList = new HashMap<Integer, LineItem>();
 		this.payment = null;
 		this.dateTime = null;
 		this.shippingAddr = null;
+		logger.info("New sale (id " + this.saleID + ") created for user " + custID);
 	}
 	
 	/**
@@ -53,11 +85,13 @@ public class Sale {
 				if(Catalogue.getInstance().checkAmount(itemID, quantity + itemList.get(itemID).getQuantity())) {
 					itemList.get(itemID).addSome(quantity);
 					success = true;
+					logger.info("SALE " + this.saleID + " of user " + this.customerID + ": Increased quantity of " + itemID + " to " + quantity);
 				}
 			} else {
 				if(Catalogue.getInstance().checkAmount(itemID, quantity)){
 					itemList.put(itemID, new LineItem(quantity, itemID));
 					success = true;
+					logger.info("SALE " + this.saleID + " of user " + this.customerID + ": Added " + quantity  + " " + itemID + "s (new item)");
 				}
 			}
 		}
@@ -72,6 +106,7 @@ public class Sale {
 	public void removeItem(int itemID) {
 		if(!this.finalized && itemList.containsKey(itemID)) {
 			itemList.remove(itemID);
+			logger.info("SALE " + this.saleID + " of user " + this.customerID + ": Removed " + itemID + " from sale");
 		}
 	}
 	
@@ -85,6 +120,7 @@ public class Sale {
 	public boolean editQuantity(int itemID, int newQuantity) {
 		if(!this.finalized && Catalogue.getInstance().checkAmount(itemID, newQuantity)) {
 			itemList.get(itemID).setQuantity(newQuantity);
+			logger.info("SALE " + this.saleID + " of user " + this.customerID + ": Changed quantity of " + itemID + " to " + newQuantity);
 			return true;
 		}
 		return false;
@@ -98,6 +134,7 @@ public class Sale {
 	public void applyPromoCode(String code) {
 		if(!this.finalized) {// && this.checkPromoCode(code)
 			//guaranteed to be the right length
+			logger.info("SALE " + this.saleID + " of user " + this.customerID + ": Attempting to add promo code " + code + " to any items in sale that can");
 			for(Integer i : this.itemList.keySet()) {
 				this.itemList.get(i).addPromoCode(code);
 			}
@@ -138,6 +175,7 @@ public class Sale {
 		for(LineItem li : itemList.values()) {
 			total += li.getTotalPrice();
 		}
+		
 		return total;
 	}
 
@@ -180,9 +218,11 @@ public class Sale {
 	 */
 	public boolean finalizePayment(){
 		if(!this.finalized && this.checkPaymentAmount() && this.shippingAddr != null && this.customerID != -1) {
+			logger.fine("SALE " + this.saleID + " of user " + this.customerID + ": FINIALIZING PAYMENT");
 			this.finalized = true;
 			//decrease quantity from catalogue
 			for(LineItem li : this.itemList.values()) {
+				logger.fine("SALE " + this.saleID + " of user " + this.customerID + ": Decreasing item " + li.getItemID() + " by " + li.getQuantity());
 				Catalogue.getInstance().decreaseQuantity(li.getItemID(), li.getQuantity());
 			}
 			this.dateTime = new Date();
@@ -195,6 +235,7 @@ public class Sale {
 			Set<Administrator> adminsChanged = new HashSet<>();
 			Set<Customer> custChanged = new HashSet<>();
 			
+			logger.fine("SALE " + this.saleID + " of user " + this.customerID + ": Adding this sale to the vendors that sold it.");
 			this.itemList.entrySet().stream().forEach(e -> {
 				String id = (c.getItem(e.getValue().getItemID()).getVendorID() + "");
 				switch(id.charAt(0)) {
@@ -208,6 +249,7 @@ public class Sale {
 							adminsChanged.add(a);
 						} else {
 							//TODO:: logger log severe couldn't find administrator
+							logger.severe("SALE " + this.saleID + " of user " + this.customerID + ": COULDN'T FIND ADMINISTRATOR \'" + id + "\'");
 						}
 						break;
 					case '1':
@@ -220,11 +262,13 @@ public class Sale {
 							custChanged.add(a1);
 						} else {
 							//TODO:: logger log severe couldn't find customer
+							logger.severe("SALE " + this.saleID + " of user " + this.customerID + ": COULDN'T FIND CUSTOMER \'" + id + "\'");
 						}
 						break;
 					default:
 						//why are we even here
 						//TODO:: LOGGER SEVERE ID NOT RECOGNIZED
+						logger.severe("SALE " + this.saleID + " of user " + this.customerID + ": COULDN'T FIND USER WITH IMPROPER ID \'" + id + "\'");
 				}
 			});
 			
