66d5ff9112f6d422136fa27133dc2e6d6aaa570f
Joshua Hanscheck <Joshua_Hanscheck1@baylor.edu>
2019-04-21 04:27:12 -0500

    Created About Us and FAQ files

---
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_AboutUs.java
@@ -0,0 +1,10 @@
+package DysfunctionalDesigners.CompSciMerchStore;
+
+import java.awt.event.ActionListener;
+
+public class GUIdo_AboutUs extends GUIdo_CPanel{
+
+	GUIdo_AboutUs(final ActionListener al){
+		
+	}
+}
---
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_FAQScreen.java
@@ -0,0 +1,11 @@
+package DysfunctionalDesigners.CompSciMerchStore;
+
+import java.awt.event.ActionListener;
+
+public class GUIdo_FAQScreen extends GUIdo_CPanel{
+
+	
+	GUIdo_FAQScreen(final ActionListener al){
+		
+	}
+}
