b5f580932da4fd6d7b91293b30869efd69702fe4
etdickey <etdickey@gmail.com>
2019-04-22 14:03:25 -0500

    Initial customer tester commit.

---
+++ CompSciMerchStore/src/test/java/DysfunctionalDesigners/CompSciMerchStore/CustomerTester.java
@@ -0,0 +1,11 @@
+package DysfunctionalDesigners.CompSciMerchStore;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class CustomerTester {
+	protected Customer cust = null;
+	
+	@BeforeEach
+	void init() {
+	}
+}
