587de964e4b53a1a27d120a3777ffd1152e50b12
Joshua Hanscheck <Joshua_Hanscheck1@baylor.edu>
2019-04-21 23:42:16 -0500

    Added more of the functionality to Contact Us, finished About Us

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_AboutUs.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_AboutUs.java
@@ -1,10 +1,36 @@
 package DysfunctionalDesigners.CompSciMerchStore;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionListener;
+
+import javax.swing.JLabel;
 
 public class GUIdo_AboutUs extends GUIdo_CPanel{
 
+	JLabel l1, l2, l3;
+	
 	GUIdo_AboutUs(final ActionListener al){
 		
+		super(600);
+		
+		l1 = new JLabel("About us");
+		l1.setForeground(Color.blue);
+		l1.setFont(new Font("Arial", Font.BOLD, 20));
+		
+		l2 = new JLabel("We are Dysfunctional Designers! "
+				+ "We are Computer Science Students in CSI 3471." 
+				+ "Our team consists of 6 people: Ethan Dickey, Josh Holland,"
+				+ "Mackenna Semeyn, Em Lakin, Harrison Rogers, and Josh Hanscheck."
+				+ "You can find us in the second row on the left side during class and"
+				+ " our favorite theme when presenting is imitating the great and powerful Dr. Booth.");
+		l3 = new JLabel("About Us!");
+		
+		l2.setBounds(50, 50, 400, 500);
+		l3.setBounds(300, 50, 200, 30);
+		
+		this.add(l1);
+		this.add(l2);
+		this.add(l3);
 	}
 }
--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_FAQScreen.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_FAQScreen.java
@@ -1,11 +1,31 @@
 package DysfunctionalDesigners.CompSciMerchStore;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionListener;
+
+import javax.swing.JLabel;
 
 public class GUIdo_FAQScreen extends GUIdo_CPanel{
 
+	//whatever, text, title
+	JLabel l1, l2, l3;
 	
 	GUIdo_FAQScreen(final ActionListener al){
+		super(600);
 		
+		l1 = new JLabel("FAQ");
+		l1.setForeground(Color.blue);
+		l1.setFont(new Font("Arial", Font.BOLD, 20));
+		
+		l2 = new JLabel("");
+		l3 = new JLabel("Frequently Asked Questions!");
+		
+		l2.setBounds(50, 50, 400, 500);
+		l3.setBounds(300, 50, 200, 30);
+		
+		this.add(l1);
+		this.add(l2);
+		this.add(l3);
 	}
 }
