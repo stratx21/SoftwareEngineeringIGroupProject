8f34dcba2f71e1f4455ce557387ae4965e81d923
etdickey <etdickey@gmail.com>
2019-04-22 13:45:33 -0500

    Finished tester for Admin.

---
+++ CompSciMerchStore/src/test/java/DysfunctionalDesigners/CompSciMerchStore/AdministratorTester.java
@@ -0,0 +1,32 @@
+package DysfunctionalDesigners.CompSciMerchStore;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AdministratorTester {
+	protected Administrator admin = null;
+
+	@BeforeEach
+	void init() {
+		admin = new Administrator();
+	}
+	
+	@Test
+	void removeItemTest() {
+		//theoretically shouldn't affect the catalogue because it doesn't export
+		Catalogue c = Catalogue.getInstance();
+		assertTrue(c.getItem(0) != null);
+		
+		admin.removeItemFromCatalogue(0);
+		assertFalse(c.getItem(0).isEnabled());
+	}
+	
+	@Test
+	void testReport() {
+		assertTrue(admin.generateAllUsersReport());
+		assertTrue(admin.generateAllSalesReport());
+	}
+}
