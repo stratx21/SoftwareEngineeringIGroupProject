fcf0ebbc2a0ea6aab329023d8435158577280a24
Mackenna Semeyn <Mackenna_Semeyn1@baylor.edu>
2019-04-22 22:01:13 -0500

    enter card information field

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_Payment.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_Payment.java
@@ -2,16 +2,17 @@ package DysfunctionalDesigners.CompSciMerchStore;
 
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.DecimalFormat;
 import java.text.ParseException;
 
-import javax.swing.GroupLayout;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.text.MaskFormatter;
 
@@ -24,14 +25,16 @@ public class GUIdo_Payment extends GUIdo_CPanel implements ActionListener{
   	    this.repaint();
 	}
 	
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawRect(950, 120, 225, 250);
+		g.drawLine(1025, 170, 1105, 170);
+		g.drawLine(1025, 325, 1105, 325);
+	}
+	
 
 	private void drawScreen(Sale sale, int i, Customer customer) {
-		// TODO Auto-generated method stub
-		GroupLayout layout = new GroupLayout(this);
-		this.setLayout(layout);
-		layout.setAutoCreateGaps(true);
-		layout.setAutoCreateContainerGaps(true);
-//		DecimalFormat df2 = new DecimalFormat("0.00");
+		this.setLayout(null);
 		MaskFormatter card = null;
 		try {
 			card = new MaskFormatter("################");
@@ -62,14 +65,17 @@ public class GUIdo_Payment extends GUIdo_CPanel implements ActionListener{
 		}
 		
 		JLabel label = new JLabel("Payment Information");
+		
 		label.setHorizontalAlignment(JLabel.CENTER);
 		label.setVerticalAlignment(JLabel.CENTER);
+		label.setBounds(100, 0, 100, 100);
 		label.setFont(new Font("Cambria", Font.BOLD, 34));
-		this.add(label);
+		
 		
 		JLabel addCard = new JLabel("Add a Card");
 		addCard.setHorizontalAlignment(JLabel.LEFT);
-		addCard.setFont(new Font("Cambria", Font.BOLD, 28));
+		addCard.setSize(new Dimension(25, 10));
+		addCard.setFont(new Font("Cambria", Font.BOLD, 50));
 		
 		JLabel cardName = new JLabel("Name on Card");
 		JTextField nameOnCard = new JTextField();
@@ -89,7 +95,7 @@ public class GUIdo_Payment extends GUIdo_CPanel implements ActionListener{
 		JLabel bZip = new JLabel("Zipcode");
 		JFormattedTextField zip = new JFormattedTextField(zipFormat);
 		
-		
+		GUIdo_CButton placeOrder = new GUIdo_CButton(this.getWidth() / 4, this.getHeight() / 5, 150, 50, "Place Order");
 		GUIdo_CButton addCardButton = new GUIdo_CButton(GUIdo_CButton.LEADING, GUIdo_CButton.LEADING, 25, 10, "Add Card");
 		
 		addCardButton.setActionListener_clicked(new ActionListener() {
@@ -108,24 +114,70 @@ public class GUIdo_Payment extends GUIdo_CPanel implements ActionListener{
 		
 		});
 		
-		layout.setVerticalGroup(layout.createSequentialGroup()
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-						.addComponent(cardName)
-						.addComponent(nameOnCard))
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-						.addComponent(cardNum)
-						.addComponent(numberOnCard))
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-						.addComponent(cvv)
-						.addComponent(cvvNum))
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-						.addComponent(addCardButton))
-		);
+		placeOrder.setActionListener_clicked(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(isEnabled()) {
+					System.out.println("button enabled");
+					JOptionPane.showMessageDialog(null, (Object) "Thank you for your order!");
+				}else {
+					JOptionPane.showMessageDialog(null, (Object) "Please enter all information"); // not appearing
+				}
+				
+			}
+			
+		});
 		
+		this.add(placeOrder);
+		
+		GridBagLayout gbl = new GridBagLayout();
+		//gbl.removeLayoutComponent(label);
+		this.setLayout(gbl);
+		this.add(label);
+		GridBagConstraints c = new GridBagConstraints();
+		c.insets = new Insets(5, 5, 0, 0);
+		c.anchor = GridBagConstraints.BELOW_BASELINE_LEADING;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		//cardName.setHorizontalTextPosition(this.getWidth() / 20);
+		this.add(cardName, c);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.BELOW_BASELINE_LEADING;
+		c.gridx = 1;
+		c.gridy = 0;
+		this.add(nameOnCard, c);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.BELOW_BASELINE_LEADING;
+		c.gridx = 0;
+		c.gridy = 1;
+		this.add(cardNum, c);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.BELOW_BASELINE_LEADING;
+		c.gridx = 1;
+		c.gridy = 1;
+		this.add(numberOnCard, c);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.BELOW_BASELINE_LEADING;
+		c.gridx = 0;
+		c.gridy = 2;
+		this.add(cvv, c);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.BELOW_BASELINE_LEADING;
+		c.gridx = 1;
+		c.gridy = 2;
+		this.add(cvvNum, c);
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.BELOW_BASELINE_LEADING;
+		c.gridx = 3;
+		c.gridy = 3;
+		this.add(addCardButton, c);
+		
+	
+		this.setVisible(true);
 		
 	}
-
-
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
