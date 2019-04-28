4c45de37919ee78f2462a8ad0307e506f9a7d420
Mackenna Semeyn <Mackenna_Semeyn1@baylor.edu>
2019-04-28 14:01:44 -0500

    added todo to link panel to addreview

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_ItemDisplay.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_ItemDisplay.java
@@ -286,6 +286,8 @@ public class GUIdo_ItemDisplay extends GUIdo_CPanel{
 			add_review_button.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					done.actionPerformed(new ActionEvent(item,ActionEvent.ACTION_PERFORMED,"add_review"));
+					// TODO: link panel to add review
+					// verify that customer has purchased item before
 				}
 			});
 			this.add(add_review_button);
