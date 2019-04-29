03da18ce23e0b642cbf46d5ffa973bb789bdc82a
Mackenna Semeyn <Mackenna_Semeyn1@baylor.edu>
2019-04-28 19:09:14 -0500

    added all components to layout

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_PreviousOrders.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_PreviousOrders.java
@@ -1,5 +1,81 @@
 package DysfunctionalDesigners.CompSciMerchStore;
 
-public class GUIdo_PreviousOrders {
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Map;
 
+import javax.swing.JLabel;
+
+public class GUIdo_PreviousOrders extends GUIdo_CPanel implements ActionListener{
+	
+	public Catalogue catalogue = new Catalogue(); // instance to use to search for item names by id
+	
+	public GUIdo_PreviousOrders(Customer customer) {
+		super();
+		if(customer.getPreviousPurchases().size() < 2) {
+			this.setPreferredSize(new Dimension(this.getWidth(), 500));
+		}else {
+			this.setPreferredSize(new Dimension(this.getWidth(), 500 * customer.getPreviousPurchases().size()));
+		}
+		this.drawScreen(customer);
+		this.repaint();
+	}
+	
+	public void drawScreen(Customer customer) {
+		GridBagLayout layout = new GridBagLayout();
+		this.setLayout(layout);
+		GridBagConstraints c = new GridBagConstraints();
+		int x = 0;
+		int y = 0;
+		
+		JLabel previousPurchases = new JLabel("Previous Purchases");
+		previousPurchases.setFont(new Font("Cambria", Font.BOLD, 34));
+		c.gridx = x;
+		c.gridy = y;
+		c.insets = new Insets(0, 10, 0 , 10);
+		c.anchor = GridBagConstraints.NORTH;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		this.add(previousPurchases, c);
+		
+		for(Sale s: customer.getPreviousPurchases()) {
+			JLabel orderOn = new JLabel("Order On: " + s.getDateTime());
+			orderOn.setFont(new Font("Cambria", Font.BOLD, 22));
+			y++;
+			c.gridy = y;
+			c.anchor = GridBagConstraints.LINE_START;
+			c.fill = GridBagConstraints.NONE;
+			this.add(orderOn, c);
+			
+			for(Map.Entry<Integer, LineItem> entry : s.getItemList().entrySet()) {
+				ItemInfo item = catalogue.getItem(entry.getValue().getItemID());
+				JLabel name = new JLabel(item.getDisplayName() + " x " + entry.getValue().getQuantity());
+				name.setFont(new Font("Cambria", Font.PLAIN, 16));
+				y++;
+				c.gridy = y;
+				c.anchor = GridBagConstraints.LINE_START;
+				c.fill = GridBagConstraints.NONE;
+				this.add(name, c);
+			}
+			
+			JLabel total = new JLabel("Total: $" + s.getTotalWithTax());
+			total.setFont(new Font("Cambria", Font.PLAIN, 16));
+			y++;
+			c.gridy = y;
+			c.anchor = GridBagConstraints.LINE_START;
+			c.fill = GridBagConstraints.NONE;
+			this.add(total, c);
+			y++; // increment twice to have extra space in between orders
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
