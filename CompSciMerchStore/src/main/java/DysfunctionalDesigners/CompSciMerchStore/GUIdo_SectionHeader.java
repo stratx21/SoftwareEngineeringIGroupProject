b7f6d9955f585aa1d65da554c595d54044afbee4
Em Lakin <emlakin21@gmail.com>
2019-04-30 16:52:26 -0500

    button hover color

--- CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_SectionHeader.java
+++ CompSciMerchStore/src/main/java/DysfunctionalDesigners/CompSciMerchStore/GUIdo_SectionHeader.java
@@ -41,49 +41,57 @@ public class GUIdo_SectionHeader extends GUIdo_CPanel{
 		section1.setActionCommand("cerny");
 		section1.setActionListener_clicked(done);
 		section1.setBackground(new Color(255,228,225));
+		section1.setHoverColor(new Color(255,192,203));
 
 		//the button for Dr. Booth's section
 		GUIdo_CButton section2 = new GUIdo_CButton(x+1*width/SECTIONS,y,width/SECTIONS,height,"Dr. Booth");
 		section2.setActionCommand("booth");
 		section2.setActionListener_clicked(done);
 		section2.setBackground(new Color(255,228,225));
+		section2.setHoverColor(new Color(255,192,203));
 
 		//the button for Professor Fry's section
 		GUIdo_CButton section3 = new GUIdo_CButton(x+2*width/SECTIONS,y,width/SECTIONS,height,"Professor Fry");
 		section3.setActionCommand("fry");
 		section3.setActionListener_clicked(done);
 		section3.setBackground(new Color(255,228,225));
+		section3.setHoverColor(new Color(255,192,203));
 
 		//the button for Dr. Hamerly's section
 		GUIdo_CButton section4 = new GUIdo_CButton(x+3*width/SECTIONS,y,width/SECTIONS,height,"Dr. Hamerly");
 		section4.setActionCommand("hamerly");
 		section4.setActionListener_clicked(done);
 		section4.setBackground(new Color(255,228,225));
+		section4.setHoverColor(new Color(255,192,203));
 
 		//the button for Bald Aars' section
 		GUIdo_CButton section5 = new GUIdo_CButton(x+4*width/SECTIONS,y,width/SECTIONS,height,"Professor Bald Aars");
 		section5.setActionCommand("baldaars");
 		section5.setActionListener_clicked(done);
 		section5.setBackground(new Color(255,228,225));
+		section5.setHoverColor(new Color(255,192,203));
 
 		//the button for Hairy Aars' section
 		GUIdo_CButton section6 = new GUIdo_CButton(x+5*width/SECTIONS,y,width/SECTIONS,height,"Professor Hairy Aars");
 		section6.setActionCommand("hairyaars");
 		section6.setActionListener_clicked(done);
 		section6.setBackground(new Color(255,228,225));
+		section6.setHoverColor(new Color(255,192,203));
 
 		//the button for Dr. Maurer's section
 		GUIdo_CButton section7 = new GUIdo_CButton(x+6*width/SECTIONS,y,width/SECTIONS,height,"Dr. Maurer");
 		section7.setActionCommand("maurer");
 		section7.setActionListener_clicked(done);
 		section7.setBackground(new Color(255,228,225));
+		section7.setHoverColor(new Color(255,192,203));
 
 		//the button for the Dysfunctional Designers section
 		GUIdo_CButton section8 = new GUIdo_CButton(x+7*width/SECTIONS,y,width/SECTIONS,height,"Dysfunctional Designers");
 		section8.setActionCommand("dys_des");
 		section8.setActionListener_clicked(done);
-//		section8.setVisible(true);
+
 		section8.setBackground(new Color(255,228,225));
+		section8.setHoverColor(new Color(255,192,203));
 
 
 		//add to the toolbar panel:
